4d3eb437a102d05c0929e9b6758d6eca656597ca
Mehedi Khan Rakib <mkrakib007@gmail.com>
2022-06-23 04:04:31 +0600

    latest update of lab-15

--- php-lab/lab-15/get.php
+++ php-lab/lab-15/get.php
@@ -2,16 +2,14 @@
 session_start();
 require_once './config/db.php';
 // extract($_POST);
-extract($_SESSION['user']);
-$sql = "SELECT * FROM users WHERE email='$email' && password='$password'";
+if (isset($_SESSION['auth_user'])) {
+    extract($_SESSION['auth_user']);
+}
+$sql = "SELECT * FROM users WHERE email='$email'";
 $result = mysqli_query($conn, $sql);
 $count = mysqli_num_rows($result);
 
-if (isset($_SESSION['user_success'])) {
-?>
-    <p><?php echo $_SESSION['user_success'] ?></p>
-<?php
-}
+
 // WHERE email='$email' && password='$password'
 if ($count === 1) {
     $row = mysqli_fetch_assoc($result);
@@ -22,14 +20,12 @@ if ($count === 1) {
             <th>Name</th>
             <th>Username</th>
             <th>Email</th>
-            <th>Password</th>
             <th>Contact No.</th>
         </tr>
         <tr>
             <td><?php echo $name; ?></td>
             <td><?php echo $username; ?></td>
             <td><?php echo $email; ?></td>
-            <td><?php echo $password; ?></td>
             <td><?php echo $contact; ?></td>
             <td><a href="logout.php">Logout</a></td>
         </tr>
